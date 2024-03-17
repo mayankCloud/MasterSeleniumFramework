a7cd73d974fbc9f57714de4ab13e2c278d6c442e
mayankCloud <cse.mayankgupta@gmail.com>
2024-03-18 01:27:06 +0530

    Generic Jackson deserialization code

--- src/test/java/org/selenium/pom/utils/JacksonUtils.java
+++ src/test/java/org/selenium/pom/utils/JacksonUtils.java
@@ -1,16 +1,14 @@
 package org.selenium.pom.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.selenium.pom.objects.BillingAddress;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class JacksonUtils {
-    public static <T>T deSerializeBillingAddress(String filename, Class<T> T) throws IOException {
-        InputStream myBillingAddressStream = JacksonUtils.class.getClassLoader().getResourceAsStream(filename);
+    public static <T>T deSerializeJSON(String filename, Class<T> T) throws IOException {
+        InputStream inputStream = JacksonUtils.class.getClassLoader().getResourceAsStream(filename);
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(myBillingAddressStream,T);
+        return objectMapper.readValue(inputStream,T);
     }
 }
