204c53d07432abe9cf739dc2245ab97df75bdad1
mayankCloud <cse.mayankgupta@gmail.com>
2024-03-17 19:09:03 +0530

    Added DefaultConstructor, Parameterized Constructor and BuilderPattern for setters.

--- src/test/java/org/selenium/pom/objects/BillingAddress.java
+++ src/test/java/org/selenium/pom/objects/BillingAddress.java
@@ -1,52 +1,71 @@
 package org.selenium.pom.objects;
 
+import org.selenium.pom.pages.CheckoutPage;
+
 public class BillingAddress {
+
+    public BillingAddress(){}
+
+    public BillingAddress(String firstName,String lastName, String addressLine, String townOrCity,String zipCode, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.addressLine = addressLine;
+        this.townOrCity = townOrCity;
+        this.zipCode = zipCode;
+        this.email = email;
+    }
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public BillingAddress setFirstName(String firstName) {
         this.firstName = firstName;
+        return this;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public BillingAddress setLastName(String lastName) {
         this.lastName = lastName;
+        return this;
     }
 
     public String getAddressLine() {
         return addressLine;
     }
 
-    public void setAddressLine(String addressLine) {
+    public BillingAddress setAddressLine(String addressLine) {
         this.addressLine = addressLine;
+        return this;
     }
 
     public String getTownOrCity() {
         return townOrCity;
     }
 
-    public void setTownOrCity(String townOrCity) {
+    public BillingAddress setTownOrCity(String townOrCity) {
         this.townOrCity = townOrCity;
+        return this;
     }
 
     public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(String zipCode) {
+    public BillingAddress setZipCode(String zipCode) {
         this.zipCode = zipCode;
+        return this;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
+    public BillingAddress setEmail(String email) {
         this.email = email;
+        return this;
     }
 
     private String firstName;
