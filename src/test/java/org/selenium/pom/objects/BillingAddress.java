71ce89d295479562023132e6fa44f0b47bcb847d
mayankCloud <cse.mayankgupta@gmail.com>
2024-03-17 18:55:19 +0530

    Added BillingAddress POJO

---
+++ src/test/java/org/selenium/pom/objects/BillingAddress.java
@@ -0,0 +1,58 @@
+package org.selenium.pom.objects;
+
+public class BillingAddress {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddressLine() {
+        return addressLine;
+    }
+
+    public void setAddressLine(String addressLine) {
+        this.addressLine = addressLine;
+    }
+
+    public String getTownOrCity() {
+        return townOrCity;
+    }
+
+    public void setTownOrCity(String townOrCity) {
+        this.townOrCity = townOrCity;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String firstName;
+    private String lastName;
+    private String addressLine;
+    private String townOrCity;
+    private String zipCode;
+    private String email;
+}
