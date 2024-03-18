ad569947baf4bfa6931b8333326ad2f75cb9bfc8
mayankCloud <cse.mayankgupta@gmail.com>
2024-03-18 23:43:22 +0530

    Updated DriverManager to support passing browser from maven command line and testng.xml

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -2,11 +2,24 @@ package org.selenium.pom.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.selenium.constants.BrowserType;
 
 public class DriverManager {
+    WebDriver driver;
 
-    public WebDriver initializeDriver(){
-        WebDriver driver = new ChromeDriver();
+    public WebDriver initializeDriver(String browser) {
+        browser = System.getProperty("browser", browser);
+        switch (BrowserType.valueOf(browser)) {
+            case CHROME -> {
+                driver = new ChromeDriver();
+            }
+            case FIREFOX -> {
+                driver = new FirefoxDriver();
+            }
+            default -> throw new IllegalStateException("Invalid browser name:" + browser);
+        }
         driver.manage().window().maximize();
         return driver;
     }
